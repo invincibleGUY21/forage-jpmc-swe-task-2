1b6c57d49d2f9bea1330bea67c8362c9b21ab63f
Om Kulpe <omkulpe1513@gmail.com>
2023-07-19 00:40:40 +0530

    Made another edit to the App.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,8 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
